eb3ad567ccbb3571252bd9c84d2d487153396df2
CoolCreasu <svenboeckx2@gmail.com>
2022-06-22 10:11:22 +0200

    Added quick safety check if nothing is passed in

--- APL3/Tokenizer.cs
+++ APL3/Tokenizer.cs
@@ -9,10 +9,13 @@ namespace APL3
     internal class Tokenizer
     {
         private readonly string _source = string.Empty;
+        private char _currentCharacter = default;
+        private int _currentIndex = default;
 
         public Tokenizer(string source)
         {
             _source = source.ReplaceLineEndings("\n");
+            _currentCharacter = _source.Length > 0 ? _source[_currentIndex] : '\0';
         }
     }
 }
